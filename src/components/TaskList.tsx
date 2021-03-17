a29b281f0b2361467fc97c209fe8394c4c44feae
Gabriel Paolini <paolini396@gmail.com>
2021-03-17 09:33:19 -0300

    feat:  create, completion and remove tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,35 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return;
+
+      const id = Math.random();
+
+      const newTask = {
+        id,
+        title: newTaskTitle,
+        isComplete: false,
+      }
+
+      setTasks(oldData => [...oldData, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+ 
+    const newTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task);
+
+    setTasks(newTask);
+  
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTask = tasks.filter(task => task.id !== id);
+
+    setTasks(newTask);
   }
 
   return (
